2d05776fbb7909301caf97b23616179552befb37
WhoisJulie007 <maydeli.castan@iest.edu.mx>
2025-09-19 17:52:46 -0600

    Initial Commit

---
+++ ViewModifierSubViewBindings/ViewModifierSubViewBindingsApp.swift
@@ -0,0 +1,17 @@
+//
+//  ViewModifierSubViewBindingsApp.swift
+//  ViewModifierSubViewBindings
+//
+//  Created by win603 on 19/09/25.
+//
+
+import SwiftUI
+
+@main
+struct ViewModifierSubViewBindingsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ViewModifierSubViewBindingsTests/ViewModifierSubViewBindingsTests.swift
@@ -0,0 +1,17 @@
+//
+//  ViewModifierSubViewBindingsTests.swift
+//  ViewModifierSubViewBindingsTests
+//
+//  Created by win603 on 19/09/25.
+//
+
+import Testing
+@testable import ViewModifierSubViewBindings
+
+struct ViewModifierSubViewBindingsTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
