4fc4f0244bf3a497cd29f61a6ea368bd3ec5c3a8
WhoisJulie007 <maydeli.castan@iest.edu.mx>
2025-09-19 17:58:41 -0600

    prueba

--- ViewModifierSubViewBindings/ContentView.swift
+++ ViewModifierSubViewBindings/ContentView.swift
@@ -13,7 +13,7 @@ struct ContentView: View {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("Hello, world! Esta es una prueba de ViewModifierSubViewBindings.")
         }
         .padding()
     }
